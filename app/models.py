d10695e6327c508b5265cb4e9f6b807a102240aa
OvidiuCraiu <136699597+OvidiuCraiu@users.noreply.github.com>
2024-11-19 14:03:08 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     # Add this line to create the relationship
-    contracts = db.relationship('Contract', backref='author', lazy='dynamic')
+    contracts = db.relationship('Contract', back_populates='author', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
