b263eee489e8d95a9c73d4b55944d7688bf83cac
OvidiuCraiu <136699597+OvidiuCraiu@users.noreply.github.com>
2024-11-19 14:08:09 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, login_required, logout_user, current_user
 from .models import User, Contract
 from .forms import LoginForm, RegistrationForm, ContractForm, SearchForm
+from .forms import ContractCreationForm
 from . import db
 import os
 from flask import send_file
