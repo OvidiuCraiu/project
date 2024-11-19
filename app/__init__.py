5034fefd92d8832153b26d4d82866f1516cad56b
OvidiuCraiu <136699597+OvidiuCraiu@users.noreply.github.com>
2024-11-19 13:12:31 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -15,11 +16,16 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
 
-    with app.app_context():
-        db.create_all()  # This line creates the database tables
-
     from .routes import main, auth
     app.register_blueprint(main)
     app.register_blueprint(auth)
 
     return app
+
+def init_db(app):
+    with app.app_context():
+        # Import all models here
+        from .models import User, Contract
+        # Create database tables
+        db.create_all()
+        print("Database tables created.")
